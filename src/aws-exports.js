16ad82ceec406421496e1dfdf9b45ade5224dba1
Fraoucene <fraoucene.guezout@gmail.com>
2020-10-03 15:05:48 +0200

    add graphql api

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,8 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "eu-west-2",
     "aws_user_pools_id": "eu-west-2_VYBr5cRZk",
     "aws_user_pools_web_client_id": "24429g9f7nhmro4ro7u1g2l5dd",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://so3bqqcr5fguhh24v7g2l6frf4.appsync-api.eu-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-37cj6srnbrehpjzhc7joycxcg4"
 };
-
 
 export default awsmobile;
